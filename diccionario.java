77cb7b9137942e9f5a4dbf11e6b1036f1c9cdfbf
Rodrigo Márquez <44romanu@gmail.com>
2021-04-16 23:45:43 -0500

    primer commit

---
+++ diccionario.java
@@ -0,0 +1,23 @@
+//transition table
+HashTable<String,HashTable<String,String> transitionTable;
+
+
+public String transitionFunction(String state, String character){
+    return transitionTable.get(state).get(character);
+}
+
+public extendedTransitionFunctioin(String state, String chars){
+    //empty string
+    if (chars.length()==0){
+        return state;
+    } else{
+        if(chars.length()==1){
+            return transitionFunction(state,chars);
+        }else{
+            String firstPart = chars.substring(chars.length()-1);
+            String lastChar = chars.charAt(chars.length()-1)+"";
+
+            transitionFunction(extendedTransitionFunction(state,firstPart),lastChar);
+        }
+    }
+}
